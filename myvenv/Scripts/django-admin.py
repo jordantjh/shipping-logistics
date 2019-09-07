af70938a3ba6f39f142b1ecf0fb9edc0f047975c
KarloF <karloantofernandez@gmail.com>
2019-09-07 18:27:59 -0400

    windows myvenv

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\work\documents\examples\shipping-logistics\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
