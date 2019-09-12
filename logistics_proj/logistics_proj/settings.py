a8d284a7e871c610f9823d110540aaae6c24d7d8
jordantjh <jordantjh@hotmail.com>
2019-09-12 10:22:09 -0400

    add required login to internal system

--- logistics_proj/logistics_proj/settings.py
+++ logistics_proj/logistics_proj/settings.py
@@ -135,3 +135,6 @@ SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/login/'
--- logistics_proj/service_providers/urls.py
+++ logistics_proj/service_providers/urls.py
@@ -1,20 +1,27 @@
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib.auth.decorators import login_required
+
 from . import views as spViews
 
 app_name = 'service_providers'
 urlpatterns = [
-    path('', spViews.contractsView, name="contracts"),
-    path('contracts/<int:contract_id>/milestones',spViews.milestonesView, name="milestones"),
-    path('contracts/<int:contract_id>/', spViews.contractsDetailsView,name="contracts_details"),
-    path('sp/', spViews.service_pView, name="service_p"),
-    path('sp/notes/<int:note_id>/', spViews.noteDetailsView, name="note_details"),
-    path('sp/notes/', spViews.notesView, name="notes_list"),
-    path('sp/contacts/', spViews.contactsView, name="contacts_list"),
-    path('sp/contacts/<int:contact_id>/', spViews.contactDetailsView, name="contact_details"),
-    path('sp/notes/new/', spViews.noteAdd, name="note_new"),
-    path('sp/contacts/new/', spViews.contactAdd, name="contacts_new"),
-    path('passwordchanged', spViews.passwordchangedView, name="passwordchanged"),
+    path('', login_required(spViews.contractsView), name="contracts"),
+    path('contracts/<int:contract_id>/milestones',
+         login_required(spViews.milestonesView), name="milestones"),
+    path('contracts/<int:contract_id>/',
+         login_required(spViews.contractsDetailsView), name="contracts_details"),
+    path('sp/', login_required(spViews.service_pView), name="service_p"),
+    path('sp/notes/<int:note_id>/',
+         login_required(spViews.noteDetailsView), name="note_details"),
+    path('sp/notes/', login_required(spViews.notesView), name="notes_list"),
+    path('sp/contacts/', login_required(spViews.contactsView), name="contacts_list"),
+    path('sp/contacts/<int:contact_id>/',
+         login_required(spViews.contactDetailsView), name="contact_details"),
+    path('sp/notes/new/', login_required(spViews.noteAdd), name="note_new"),
+    path('sp/contacts/new/', login_required(spViews.contactAdd), name="contacts_new"),
+    path('passwordchanged', login_required(
+        spViews.passwordchangedView), name="passwordchanged"),
 
 ]
